d5270a2d25d1b0089412096ac3f3c553feabb330
araujo-lorena <lorena.araujo1.@faculdadegran.edu.br>
2025-11-11 18:23:04 -0300

    adiciona:exercícios sobre estrutura de dados(tuplas) em python

---
+++ atividades_python/tuplas/lista_preco.py
@@ -0,0 +1,8 @@
+'''
+Crie um programa que tenha uma tupla única
+com nomes de produtos e seus respectivos precos,
+na sequência
+
+No final, mostre uma listagem de precos,
+organizando os dados em forma tabular
+'''
---
+++ atividades_python/tuplas/times_futebol.py
@@ -0,0 +1,10 @@
+'''
+Crie uma tupla preenchida com os 20 primeiros colocados
+da Tabela do Campeonato Brasileiro,
+na ordem de colocação.
+Depois mostre:
+a) apenas os 5 primeiros colocados
+b) os últimos 4 colocados da tabela
+c) uma lista com os times em ordem alfabética
+d) em que posição na tabela está o time da Chapecoense
+'''
